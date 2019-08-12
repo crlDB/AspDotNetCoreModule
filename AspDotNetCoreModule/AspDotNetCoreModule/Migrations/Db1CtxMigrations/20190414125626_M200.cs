60870041576b933bbb80742843fecebf1d291311
Carl <carl@nuyttensautomatisatie.be>
2019-08-12 15:50:36 +0200

    -

--- AspDotNetCoreModule/AspDotNetCoreModule/Migrations/Db1CtxMigrations/20190414125626_M200.cs
+++ AspDotNetCoreModule/AspDotNetCoreModule/Migrations/Db1CtxMigrations/20190414125626_M200.cs
@@ -7,42 +7,10 @@ namespace AspDotNetCoreWeb.Migrations.Db1CtxMigrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Table1",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    String1 = table.Column<string>(maxLength: 50, nullable: true),
-                    Num1 = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Table1", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Table2",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    String1 = table.Column<string>(maxLength: 50, nullable: true),
-                    Num1 = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Table2", x => x.Id);
-                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Table1");
-
-            migrationBuilder.DropTable(
-                name: "Table2");
         }
     }
 }
